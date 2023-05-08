ff243549f318fa7cdb70fdbe39fcac38ebf694a1
gialexan <alexromani.sc@gmail.com>
2023-05-08 17:16:43 -0300

    add func init_forks

--- src/main.c
+++ src/main.c
@@ -6,24 +6,26 @@
 /*   By: gialexan <gialexan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 10:41:24 by gialexan          #+#    #+#             */
-/*   Updated: 2023/05/05 19:14:31 by gialexan         ###   ########.fr       */
+/*   Updated: 2023/05/08 17:15:59 by gialexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-t_philo *create_philoshopers(t_context *ctx)
+t_philo *create_philos(t_context *ctx, t_fork *forks)
 {
     int     i;
     t_philo *philos;
 
     i = -1;
     philos = malloc(ctx->num_of_philo * sizeof(t_philo));
-    while (++i <= ctx->num_of_philo)
+    if (!philos)
+        return (NULL);
+    while(++i < ctx->num_of_philo)
     {
-        philos[i].test = i + 1;
-        philos[i].fork_right;
-        printf("coloquei %d\n", i + 1);
+        philos[i].id = -1;
+        philos[i].fork_right = NULL;
+        philos[i].fork_left = NULL;
     }
     return (philos);
 }
@@ -39,20 +41,26 @@ t_context   init_context(char **argv)
     return (ctx);
 }
 
+t_fork  *init_forks(t_context *ctx)
+{
+    int     i;
+    t_fork  *forks;
+
+    i = -1;
+    forks = malloc(ctx->num_of_philo * sizeof(t_fork));
+    if (!forks)
+        return (NULL);
+    while(++i < ctx->num_of_philo)
+        pthread_mutex_init(&forks[i], NULL);
+}
+
 int main(int argc, char **argv)
 {
     t_context   ctx;
     t_philo     *philos;
+    t_fork      *forks;
 
     ctx = init_context(argv);
-    philos = create_philoshopers(&ctx);
-
-
-    int i = 0;
-    while (i <= ctx.num_of_philo)
-    {
-        printf("test: %d\n", philos[i].test);
-        i++;
-    }
-
+    forks = init_forks(&ctx);
+    philos = create_philos(&ctx, forks);
 }
--- src/philo.h
+++ src/philo.h
@@ -6,7 +6,7 @@
 /*   By: gialexan <gialexan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 10:41:36 by gialexan          #+#    #+#             */
-/*   Updated: 2023/05/05 17:18:35 by gialexan         ###   ########.fr       */
+/*   Updated: 2023/05/08 15:20:16 by gialexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,9 @@ typedef pthread_mutex_t t_fork;
 
 typedef struct s_philo
 {
-    int test;
     pthread_t       id;
     pthread_mutex_t *fork_left;
     pthread_mutex_t *fork_right;
-
 }   t_philo;
 
 typedef struct s_context
@@ -37,9 +35,8 @@ typedef struct s_context
     int     time_to_eat;
     int     time_to_sleep;
     int     num_of_time_eat;
-    t_philo **philos;
-    
-} t_context;
+
+}   t_context;
 
 
 #endif
