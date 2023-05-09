b3d7f98594657e9342b115caa7bc95ea4083dfa9
gialexan <alexromani.sc@gmail.com>
2023-05-09 15:49:26 -0300

    add get_time func & test philo mutex for dinner

--- src/main.c
+++ src/main.c
@@ -6,12 +6,11 @@
 /*   By: gialexan <gialexan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 10:41:24 by gialexan          #+#    #+#             */
-/*   Updated: 2023/05/08 19:47:29 by gialexan         ###   ########.fr       */
+/*   Updated: 2023/05/09 15:35:18 by gialexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-#include <stdio.h>
 
 
 int ft_min(int a, int b)
@@ -28,10 +27,26 @@ int ft_max(int a, int b)
     return (b);
 }
 
+t_context   **get_ctx(void)
+{
+    static t_context *ctx;
+
+    return (&ctx);
+}
+
+void    save_ctx_ref(t_context *ctx)
+{
+    t_context   **save_ctx;
+    
+    save_ctx = get_ctx();
+    *save_ctx = ctx;
+}
+
 t_philo *create_philos(t_context *ctx, t_fork *forks)
 {
-    int     i;
-    t_philo *philos;
+    int         i;
+    t_philo     *philos;
+    const char  *names[] = {"Sócrates", "Platão", "Aristóteles"};
 
     i = -1;
     philos = malloc(ctx->num_of_philo * sizeof(t_philo));
@@ -39,7 +54,8 @@ t_philo *create_philos(t_context *ctx, t_fork *forks)
         return (NULL);
     while(++i < ctx->num_of_philo)
     {
-        philos[i].id = i;
+        philos[i].id = -1;
+        philos[i].name = names[i];
         philos[i].fork_first = &forks[ft_min(i, (i + 1) % ctx->num_of_philo)];
         philos[i].fork_second = &forks[ft_max(i, (i + 1) % ctx->num_of_philo)];
     }
@@ -50,14 +66,15 @@ t_context   init_context(char **argv)
 {
     t_context   ctx;
 
-    ctx.num_of_philo = 3;
-    ctx.time_to_eat = 2;
-    ctx.time_to_sleep = 5;
-    ctx.num_of_time_eat = 2;
+    ctx.num_of_philo = 2;
+    ctx.time_to_eat = 200;
+    ctx.time_to_sleep = 100;
+    ctx.num_of_time_eat = 3;
+    gettimeofday(&ctx.start_time, NULL);
     return (ctx);
 }
 
-t_fork  *init_forks(t_context *ctx)
+t_fork  *create_forks(t_context *ctx)
 {
     int     i;
     t_fork  *forks;
@@ -68,44 +85,94 @@ t_fork  *init_forks(t_context *ctx)
         return (NULL);
     while(++i < ctx->num_of_philo)
         pthread_mutex_init(&forks[i], NULL);
+    return (forks);
 }
 
-// int main(int argc, char **argv)
+int get_time(t_context *ctx)
+{
+    t_time      end_time;
+    int         current_time;
+
+    gettimeofday(&end_time, NULL);
+    current_time = end_time.tv_sec - ctx->start_time.tv_sec;
+    return (current_time);
+}
+
+void    *routine(void *arg)
+{
+    int         i;
+    t_context   *ctx;
+    t_philo     *philo;
+    
+    i = -1;
+    ctx = *get_ctx();
+    philo = (t_philo *)arg;
+    while (++i < ctx->num_of_time_eat)
+    {
+        pthread_mutex_lock(philo->fork_first);
+        pthread_mutex_lock(philo->fork_second);
+
+        printf("%d Philosopher %s is eating\n", get_time(ctx), philo->name);
+        sleep(2);
+
+        pthread_mutex_unlock(philo->fork_first);
+        pthread_mutex_unlock(philo->fork_second);
+
+        printf("%d Philosopher %s is sleeping\n", get_time(ctx), philo->name);
+        sleep(1);
+
+        printf("%d Philosopher %s is thinking\n", get_time(ctx), philo->name);
+    }
+}
+
+void    begin_dinner(t_context *ctx, t_philo *philos)
+{
+    int i;
+
+    i = -1;
+    save_ctx_ref(ctx);
+    while (++i < ctx->num_of_philo)
+        pthread_create(&philos[i].id, NULL, &routine, &philos[i]);
+}
+
+int main(int argc, char **argv)
+{
+    t_context   ctx;
+    t_philo     *philos;
+    t_fork      *forks;
+
+    ctx = init_context(argv);
+    forks = create_forks(&ctx);
+    philos = create_philos(&ctx, forks);
+    begin_dinner(&ctx, philos);
+
+    for (int i = 0; i < ctx.num_of_philo; i++)
+        pthread_join(philos[i].id, NULL);
+}
+
+
+
+
+
+// int main()
 // {
-//     t_context   ctx;
-//     t_philo     *philos;
-//     t_fork      *forks;
+//     t_time start, end;
 
-//     ctx = init_context(argv);
-//     forks = init_forks(&ctx);
-//     philos = create_philos(&ctx, forks);
+//     gettimeofday(&start, NULL);
 
-//     int i = -1;
-//     while (++i < ctx.num_of_philo)
-//         printf("id: %ld / fork_right: %p / fork_left: %p\n", philos[i].id, (void *)philos[i].fork_right, (void *)philos[i].fork_left);
+//     while (1)
+//     {
+//         sleep(3);
+//         gettimeofday(&end, NULL);
+//         printf("The elapsed time is %ld seconds\n", end.tv_sec - start.tv_sec);
+//     }
+//     return (0);
 // }
 
-#include <sys/time.h>
 
-int main()
-{
-    struct timeval start, end;
- 
-    gettimeofday(&start, NULL);
-    gettimeofday(&end, NULL);
- 
-    long seconds = (end.tv_sec - start.tv_sec);
- 
-    printf("The elapsed time is %ld seconds\n", seconds);
-    sleep(5);
-    gettimeofday(&end, NULL);
-    seconds = (end.tv_sec - start.tv_sec);
-    printf("The elapsed time is %ld seconds\n", seconds);
-    sleep(1);
-    gettimeofday(&end, NULL);
-    seconds = (end.tv_sec - start.tv_sec);
-    printf("The elapsed time is %ld seconds\n", seconds);
- 
-    return 0;
-}
+/* teste sobre qual garfo está disponível para cada philo.
 
+     int i = -1;
+    while (++i < ctx.num_of_philo)
+        printf("id: %ld / fork_right: %p / fork_left: %p\n", philos[i].id, (void *)philos[i].fork_first, (void *)philos[i].fork_second);
+*/
--- src/philo.h
+++ src/philo.h
@@ -6,7 +6,7 @@
 /*   By: gialexan <gialexan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 10:41:36 by gialexan          #+#    #+#             */
-/*   Updated: 2023/05/08 19:29:59 by gialexan         ###   ########.fr       */
+/*   Updated: 2023/05/09 15:15:19 by gialexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,38 @@
 
 #include <time.h>
 #include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <sys/time.h>
 
 typedef pthread_mutex_t t_fork;
+typedef struct timeval  t_time;
+
+typedef enum s_status
+{
+    EATING,
+    THINKING,
+    SLEEPING,
+}   t_status;
 
 typedef struct s_philo
 {
+    const char  *name;
     pthread_t   id;
     t_fork      *fork_first;
     t_fork      *fork_second;
+    t_status    philo_action;
 }   t_philo;
 
 typedef struct s_context
 {
+    t_time  start_time;
     int     num_of_philo;
     int     time_to_eat;
+    int     time_to_die;
     int     time_to_sleep;
     int     num_of_time_eat;
-
 }   t_context;
 
 
